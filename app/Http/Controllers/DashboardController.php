b2fac671fe943abaf03bfe7a4ed189294a20daa6
andyzamroji <andyzamroji@gmail.com>
2019-05-09 12:56:43 +0700

    part 22 a

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Siswa;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboards.index');
+        $siswa = Siswa::all();
+        $siswa->map(function($s){
+            $s->rataRataNilai = $s->rataRataNilai();
+            return $s;
+        });
+
+        $siswa = $siswa->sortByDesc('rataRataNilai')->take(5);
+        return view('dashboards.index',['siswa' => $siswa]);
     }
 }
--- resources/views/dashboards/index.blade.php
+++ resources/views/dashboards/index.blade.php
@@ -1,39 +1,46 @@
 @extends('layouts.master')
 
 @section('content')
-    <!-- TABBED CONTENT -->
-    <div class="panel">
-								<div class="panel-heading">
-									<h3 class="panel-title">Mata Pelajaran</h3>
-								</div>
-								<div class="panel-body">
-									<table class="table table-striped table-hover">
-										<thead>
-											<tr>
-												<th>KODE</th>
-												<th>NAMA</th>
-												<th>SEMESTER</th>
-												<th>NILAI</th>
-												<th>GURU</th>
-												<th>AKSI</th>
-											</tr>
-										</thead>
-										<tbody>
-											@foreach($siswa->mapel as $mapel)
-											<tr>
-												<td>{{$mapel->kode}}</td>
-												<td>{{$mapel->nama}}</td>
-												<td>{{$mapel->semester}}</td>
-												<td><a href="#" class="nilai" data-type="text" data-pk="{{$mapel->id}}" data-url="/api/siswa/{{$siswa->id}}/editnilai" data-title="Masukkan Nilai">{{$mapel->pivot->nilai}}</a></td>
-												<td><a href="/guru/{{$mapel->guru_id}}/profile">{{$mapel->guru->nama}}</a></td>
-												<td><a href="/siswa/{{$siswa->id}}/{{$mapel->id}}/deletenilai" class="btn btn-danger btn-sm"
-                        onclick="return confirm('Apakah Anda Yakin akan menghapus?')">Delete</a></td>
-											</tr>
-											@endforeach
-										</tbody>
-									</table>
-								</div>
-							</div>
-									
-								<!-- END TABBED CONTENT -->
+<div class="main">
+  <div class="main-content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-6"> 
+            <!-- TABBED CONTENT -->
+            <div class="panel">
+				<div class="panel-heading">
+					<h3 class="panel-title">Rangking 5 Besar</h3>
+				</div>
+				<div class="panel-body">
+				    <table class="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th>Rangking</th>
+                                <th>Nama</th>
+                                <th>Nilai</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @php
+                                $rangking = 1;
+                            @endphp
+                            @foreach($siswa as $s)
+                            <tr>
+                                <td>{{$rangking}}</td>
+                                <td>{{$s->nama_depan}} {{$s->nama_belakang}}</td>
+                                <td>{{$s->rataRataNilai}}</td>
+                            </tr>
+                            @php
+                            $rangking++;
+                            @endphp
+                            @endforeach
+                        </tbody>
+					    </table>
+				    </div>
+			    </div>
+            </div>
+		</div>
+    </div>
+</div>
+				<!-- END TABBED CONTENT -->
 @stop
